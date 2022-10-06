60d15f3c4029914980d7889cb5a7e77bbc0a24cf
Ajay Talanki <77760044+ajaytal@users.noreply.github.com>
2022-10-06 15:38:25 -0700

    Update fiscas.cpp

--- fiscas.cpp
+++ fiscas.cpp
@@ -6,8 +6,6 @@
 #include <vector>
 #include <iomanip>
 
-//MUST CHECK FOR INVALID INSTRUCTIONS IN PASS 1
-
 std::string lower(std::string str){
     for(char& c : str)
         c = tolower(c);
@@ -41,6 +39,8 @@ void parse(std::string& str, const std::unordered_set<std::string>& instructions
 }
 
 void registerCheck(const std::string& reg, const std::unordered_map<std::string, int>& registers){
+    if(reg.empty())
+        return;
     if(registers.find(reg) == registers.end())
         std::cout << reg << " is not a valid register." << std::endl;
 }
@@ -60,7 +60,9 @@ int main(int argc, char** argv) {
     std::map<std::string, int> symbolTable;
     std::unordered_set<std::string> instructions{"not", "and", "add", "bnz"};
     std::unordered_map<std::string, int> registers{{"r0", 0}, {"r1", 1}, {"r2", 2}, {"r3", 3}};
-    std::vector<std::string> lines;
+    std::vector<std::string> assembly;
+    std::vector<std::string> hex;
+    std::vector<std::pair<std::string, int>> orderedSymbolTable;
     while(std::getline(file, line)){
         line = lower(line);
         std::stringstream parser(line);
@@ -70,13 +72,14 @@ int main(int argc, char** argv) {
                 std::cout <<  "Label " << word << "is already defined." << std::endl;
             else {
                 symbolTable.insert(std::make_pair(word.substr(0, word.size() - 1), address));
+                orderedSymbolTable.emplace_back(std::make_pair(word.substr(0, word.size() - 1), address));
                 if((parser >> word) and instructions.find(word) != instructions.end()) {  // there is an instruction
-                    lines.emplace_back(line);
+                    assembly.emplace_back(line);
                     address++;
                 }
             }
         } else if(instructions.find(word) != instructions.end()){
-            lines.emplace_back(line);
+            assembly.emplace_back(line);
             address++;
         }
     }
@@ -86,12 +89,23 @@ int main(int argc, char** argv) {
     out.open("fiscas.h");
     out << "v2.0 raw" << std::endl;
     int binary = 255;
-    std::string Rd, Rm, Rn;
-    for(int i = 0; i < lines.size(); i++){
-        parse(lines[i], instructions);
-        std::stringstream parser(lines[i]);
+    std::string Rd, Rm, Rn, target;
+    for(int i = 0; i < assembly.size(); i++){
+        parse(assembly[i], instructions);
+        std::stringstream parser(assembly[i]);
         binary = 255;
         parser >> word;
+        if(instructions.find(word) == instructions.end())
+            std::cout << word << " is an invalid instruction" << std::endl;
+        if (word == "bnz") {
+            parser >> target;
+            if(symbolTable.find(target) != symbolTable.end()) {
+                binary = symbolTable[target];
+                binary |= (3 << 6);
+            } else
+                std::cout <<  "Label " << target << " is undefined." << std::endl;
+            goto end;
+        }
         parser >> Rd;
         parser >> Rn;
         parser >> Rm;
@@ -113,18 +127,24 @@ int main(int argc, char** argv) {
             changeBits(binary, 4, registers[Rn]);
             changeBits(binary, 2, 00);
             changeBits(binary, 0, registers[Rd]);
-        } else if (word == "bnz") {
-            if(symbolTable.find(Rd) != symbolTable.end()) {
-                binary = symbolTable[Rd];
-                binary |= (3 << 6);
-            } else
-                std::cout <<  "Label " << Rd << " is not in the symbol table." << std::endl;
         }
+        end:
         std::ostringstream oss;
         oss << std::hex << std::setw(2) << std::setfill('0') << binary;
-        out << upper(oss.str()) << std::endl;
+        std::string hexCode = upper(oss.str());
+        out << hexCode << std::endl;
+        hex.emplace_back(hexCode);
     }
-    out.close();
-    file.close();
+    std::string argv3 = argv[3];
+    if(argc > 3 and argv3 == "-l"){ // -l flag
+        std::cout << "*** LABEL LIST ***" << std::endl;
+        for(int i = 0; i < orderedSymbolTable.size(); i++)
+            std::cout << orderedSymbolTable[i].first << "   " << std::setw(2) << std::setfill('0') << orderedSymbolTable[i].second << std::endl;
+        std::cout << "*** MACHINE PROGRAM ***" << std::endl;
+        for(int i = 0; i < hex.size(); i++){
+            std::cout << std::setw(2) << std::setfill('0') << i;
+            std::cout << ":" << hex[i] << "   " << assembly[i] << std::endl;
+        }
+    }
     return 0;
 }
