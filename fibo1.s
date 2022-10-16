3c62996a145b6100703df7da88c2ef3426dac6d6
Ajay Talanki <77760044+ajaytal@users.noreply.github.com>
2022-10-16 13:55:43 -0700

    Update fibo1.s

--- fibo1.s
+++ fibo1.s
@@ -1,12 +1,13 @@
-start: not r1 r0
-       and r0 r1 r0
-       not r1 r0
-       add r2 r1 r1
-       not r1 r2
-       and r3 r0 r0
-       and r3 r1 r1
-loop:  and r2 r1 r1
-       add r3 r1 r0
-       and r1 r3 r3
-       and r0 r2 r2
+start:
+       not r1 r0     ;  r1 = not garbage
+       and r0 r1 r0  ;  r0 = 0
+       not r1 r0     ;  r1 = FF
+       add r2 r1 r1  ;  r2 = FE
+       not r1 r2     ;  r1 = 1
+       and r3 r0 r0  ;  display 0
+       and r3 r1 r1  ;  display 1
+loop:
+       add r3 r1 r0  ;  r3 = r1 + r0
+       and r0 r1 r1  ;  r0 = r1
+       and r1 r3 r3  ;  r1 = r3
        bnz loop
