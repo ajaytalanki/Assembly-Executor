c5c7b75d3de321ed74af6c90bcb2cd3df373af1d
Ajay Talanki <77760044+ajaytal@users.noreply.github.com>
2023-08-21 17:29:50 -0700

    Update fiscas.cpp

--- fiscas.cpp
+++ fiscas.cpp
@@ -7,18 +7,21 @@
 #include <iomanip>
 #include <cstring>
 
+/* converts instruction to lowercase */
 std::string lower(std::string str){
   for(char& c : str)
     c = (char)tolower(c);
   return str;
 }
 
+/* modifies specified bits of a binary string */
 void changeBits(int& n, const int& pos, const int& change){
   int mask = 0b11 << pos;
   int temp = ((n & ~mask) | (change << pos));
   n &= temp;
 }
 
+// trims whitespace of instruction and deletes comments */
 void parse(std::string& s,const std::unordered_set<std::string>& instructions){
   std::stringstream parser(s);
   std::string word;
@@ -33,16 +36,36 @@ void parse(std::string& s,const std::unordered_set<std::string>& instructions){
     s = s.substr(0, index);
 }
 
-void registerCheck(const std::string& reg, const std::unordered_map<std::string,
-                   int>& registers){
+/* checks to see if register is one of the four valid registers */
+void registerCheck(const std::string& reg, const std::unordered_map<std::string, int>& registers){
   if(reg.empty())
     return;
   if(registers.find(reg) == registers.end())
     throw std::runtime_error(reg + " is not a valid register");
 }
 
+/* if -l flag is specified, print a list of labels and machine code of each instruction */
+void displayListing(int argc, char** argv, const std::map<std::string, int>&
+    symbolTable, const std::vector<std::string>& hex, const std::vector<std::string>& assembly){
+  if((argc == 3 and strcmp(argv[2], "-l") == 0)
+      or (argc == 4 and strcmp(argv[3], "-l") == 0)){
+    std::cout << "*** LABEL LIST ***" << std::endl;
+    for(const auto& pair : symbolTable)
+      std::cout << pair.first << "   " << std::setw(2) << std::setfill('0')
+                << pair.second << std::endl;
+    std::cout << std::endl;
+    std::cout << "*** MACHINE PROGRAM ***" << std::endl;
+    for(int i = 0; i < hex.size(); i++){
+      std::cout << std::setw(2) << std::setfill('0') << std::hex <<
+                std::uppercase << i;
+      std::cout << ":" << hex[i] << "   " << assembly[i] << std::endl;
+    }
+  }
+}
+
 int main(int argc, char** argv) {
-  //PASS 1
+
+  /* verifies .s file and determines if output file is specified */
   std::ifstream file;
   file.open(argv[1]);
   bool noOutputFile;
@@ -52,42 +75,48 @@ int main(int argc, char** argv) {
     std::cout << "Usage: fiscas <asm file> <hex file> [-l]" << std::endl;
     noOutputFile = true;
   }
+
+  /* map of labels and their addresses */
+  std::map<std::string, int> symbolTable;
+  /* set of instructions */
+  std::unordered_set<std::string> instructions{"not", "and", "add", "bnz"};
+  /* maps registers to decimal value */
+  std::unordered_map<std::string, int> registers{{"r0", 0}, {"r1", 1}, {"r2", 2}, {"r3", 3}};
+  /* defines list of assembly instructions and hex instructions */
+  std::vector<std::string> assembly, hex;
+
+  /* PASS 1 */
   std::string line, word;
   int address = 0;
-  std::map<std::string, int> symbolTable;
-  std::unordered_set<std::string> instructions{"not", "and", "add", "bnz"};
-  std::unordered_map<std::string, int> registers{{"r0", 0}, {"r1", 1},
-                                                 {"r2", 2}, {"r3", 3}};
-  std::vector<std::string> assembly, hex;
   while(std::getline(file, line)){
-    line = lower(line); // makes assembler case insensitive
+
+    /* lowercases string grabs each word from string */
+    line = lower(line);
     std::stringstream parser(line);
     parser >> word;
-    if(word.back() == ':'){ // label definition
+
+    /* word is a label */
+    if(word.back() == ':'){
       if(symbolTable.find(word) != symbolTable.end()) {
         std::cout <<  "Label " << word << "is already defined." << std::endl;
         exit(0);
       } else {
-        symbolTable.insert(std::make_pair
-        (word.substr(0,word.size() - 1), address));
+        symbolTable.insert(std::make_pair(word.substr(0,word.size() - 1), address));
         if((parser >> word) && instructions.find(word) != instructions.end()){
           assembly.emplace_back(line);
           address++;
         }
       }
-    } else if(instructions.find(word) != instructions.end()){
+    }
+    /* checks to see if first word in line is instruction */
+    else if(instructions.find(word) != instructions.end()){
       assembly.emplace_back(line);
       address++;
-    } else if(instructions.find(word) == instructions.end())
-      throw std::runtime_error(word + " is an invalid instruction.");
-    if(address >= 64) {
-      std::cout << "Assembler is constrained to addresses 0-63, any code after "
-                   "won't be processed." << std::endl;
-      break;
     }
+    else if(instructions.find(word) == instructions.end())
+      throw std::runtime_error(word + " is an invalid instruction.");
   }
 
-  //PASS 2
   std::ofstream out;
   if(noOutputFile)
     out.open("fiscas.hex");
@@ -96,11 +125,15 @@ int main(int argc, char** argv) {
   out << "v2.0 raw" << std::endl;
   int binary = 255;
   std::string Rd, Rm, Rn, target;
+
+  /* PASS 2 */
   for(int i = 0; i < assembly.size(); i++){
     parse(assembly[i], instructions); // gets rid of label defs + comments
     std::stringstream parser(assembly[i]);
     binary = 255;
     parser >> word;
+
+    /* if BNZ, disregard registers and shift target address into machine code */
     if (word == "bnz") {
       target = "";
       parser >> target;
@@ -113,58 +146,52 @@ int main(int argc, char** argv) {
         throw std::runtime_error("Label " + target + " is undefined.");
       goto end;
     }
+
+    /* checks for valid register names */
     Rd = "";
     Rn = "";
     Rm = "";
     parser >> Rd;
     parser >> Rn;
     parser >> Rm;
-    registerCheck(Rd, registers); // checks for valid registers
+    registerCheck(Rd, registers);
     registerCheck(Rn, registers);
     registerCheck(Rm, registers);
+
+    /* uses bit shifting to create machine code */
     if(word == "add"){
-        if(Rd.empty() or Rn.empty() or Rm.empty())
-          throw std::runtime_error("add requires three registers");
+      if(Rd.empty() or Rn.empty() or Rm.empty())
+        throw std::runtime_error("add requires three registers");
       changeBits(binary, 6, 0);
       changeBits(binary, 4, registers[Rn]);
       changeBits(binary, 2, registers[Rm]);
       changeBits(binary, 0, registers[Rd]);
     } else if (word == "and"){
-        if(Rd.empty() or Rn.empty() or Rm.empty())
-          throw std::runtime_error("and requires three registers");
+      if(Rd.empty() or Rn.empty() or Rm.empty())
+        throw std::runtime_error("and requires three registers");
       changeBits(binary, 6, 1);
       changeBits(binary, 4, registers[Rn]);
       changeBits(binary, 2, registers[Rm]);
       changeBits(binary, 0, registers[Rd]);
     } else if (word == "not"){
-        if(!Rm.empty())
-          throw std::runtime_error("not only takes two registers");
+      if(!Rm.empty())
+        throw std::runtime_error("not only takes two registers");
       changeBits(binary, 6, 2);
       changeBits(binary, 4, registers[Rn]);
       changeBits(binary, 2, 00);
       changeBits(binary, 0, registers[Rd]);
     }
+
+    /* formats, prints, and outputs hex string to output file */
     end:
     std::ostringstream oss;
-    oss << std::hex << std::setw(2) << std::setfill('0') <<
-    std::uppercase << binary;
+    oss << std::hex << std::setw(2) << std::setfill('0') << std::uppercase << binary;
     std::string hexCode = oss.str();
     out << hexCode << std::endl;
     hex.emplace_back(hexCode);
   }
-  if((argc == 3 and strcmp(argv[2], "-l") == 0)
-  or (argc == 4 and strcmp(argv[2], "-l") == 0)){ // listing output for -l flag
-    std::cout << "*** LABEL LIST ***" << std::endl;
-    for(const auto& pair : symbolTable)
-      std::cout << pair.first << "   " << std::setw(2) << std::setfill('0')
-      << pair.second << std::endl;
-    std::cout << std::endl;
-    std::cout << "*** MACHINE PROGRAM ***" << std::endl;
-    for(int i = 0; i < hex.size(); i++){
-      std::cout << std::setw(2) << std::setfill('0') << std::hex <<
-      std::uppercase << i;
-      std::cout << ":" << hex[i] << "   " << assembly[i] << std::endl;
-    }
-  }
+
+  /* Prints out list of labels as well as machine code of each instruction */
+  displayListing(argc, argv, symbolTable, hex, assembly);
   return 0;
 }
